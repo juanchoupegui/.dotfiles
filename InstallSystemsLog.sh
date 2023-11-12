4e45e102983bd0267bd8d9d88e8320d74f4b587c
juanchoupegui <jotapeacheupe@hotmail.com>
2023-11-12 22:33:57 +0100

    initialConfig

---
+++ InstallSystemsLog.sh
@@ -0,0 +1,3 @@
+sudo apt install zsh #maybe 'curl' and 'git' too, deppending on the environment
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
